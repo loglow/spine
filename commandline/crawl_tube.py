c830e0546a7ee6b0b86acf72c4d29d4341d1308d
Bassam Kurdali <bassam@urchn.org>
2016-07-07 00:40:02 -0400

    full on

--- commandline/crawl_tube.py
+++ commandline/crawl_tube.py
@@ -6,14 +6,17 @@ BAD_FILES = [
     "gilgamesh_sim.blend", "ad.blend", "roach_smash.blend", "track_old.blend",
     "tracks.blend", "train-nizu.blend", "train.blend", "train_merge1.blend",
     "a1s02.blend", "a1s01_link_actions_a3s20.blend","a1s02gravel.blend",
-    "a1s02light.blend", "a2s00.blend", "a2s49.blend", "a2s49light.blend"]
-PATH = "/home/bassam/old/bassam/projects/hamp/tube"
+    "a1s02light.blend", "a2s00.blend", "a2s49.blend", "a2s49light.blend",
+    "a3s30crowd.blend", "meshcasher.blend", "group_override_scene.blend",
+    "gilgalight_uplight.blend", "gilgalight_backlight.blend", "gilgalight.blend",
+    "dustlocal.blend", "scenes/dev/"]
+PATH = "/home/bassam/projects/hamp/tube"
 
 def crawl_tube():
     svn = spider.ProjectTree(PATH)
     files = (f for f in svn.all_files() if all(b not in f for b in BAD_FILES))
     tube = crawler.ProjectCrawler(PATH)
-    tube.clear()
+    # tube.clear()
 
     tube.check_files(files)
     tube.save()
