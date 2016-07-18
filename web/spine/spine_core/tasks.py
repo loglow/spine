19ba9d76e93e11d21085f7282671ef2319d4d7d9
Bassam Kurdali <bassam@urchn.org>
2016-07-18 01:44:39 -0400

    getting dependencies

--- web/spine/spine_core/tasks.py
+++ web/spine/spine_core/tasks.py
@@ -7,6 +7,12 @@ from django.utils import timezone
 import datetime
 
 
+def log(text):
+    with open("/home/bassam/spine.log", "a") as myfile:
+        myfile.write("{}\n".format(text))
+
+
+
 def add_or_get_file(path, repo):
     possibles = File.objects.filter(path=path)
     if not possibles:
@@ -22,8 +28,10 @@ def add_or_get_file(path, repo):
 
 
 def add_or_get_deps(master, depend):
+    log("TRYING TO ADD DEP")
     possibles = Depend.objects.filter(master_file=master, depend_file=depend)
     if not possibles:
+        log("Adding Depend")
         entry = Depend(
             master_file=master,
             depend_file=depend,
@@ -31,8 +39,10 @@ def add_or_get_deps(master, depend):
             master_last_edited=master.last_edited,
             depend_version=depend.last_version,
             depend_last_edited=depend.last_edited)
+        log("saving dependency")
         entry.save()
     else:
+        log("alread have dep {}".format(possibles[0]))
         entry = possibles[0]
     return entry
 
@@ -49,9 +59,13 @@ def list_current(repo_id):
 @shared_task
 def dep_current(repo_id):
     repo = get_object_or_404(Repo, pk=repo_id)
+    spider = ProjectTree(repo.root_path)
     crawler = ProjectCrawler(repo.root_path)
-    for mainfile, depfiles in crawler.check_project():
+    mainfiles = spider.all_files()
+    for mainfile, depfiles in crawler.check_files(mainfiles):
+        log("GOTTEN {}, {}".format(mainfile, [d for d in depfiles]))
         main_db = add_or_get_file(mainfile, repo)
-        for depfile in depfiles:
+        for i, depfile in enumerate(depfiles):
+            log("calling adding or get file #{}".format(i))
             dep_db = add_or_get_file(depfile, repo)
-            add_or_get_deps(main_db, dep_db)
+            add_or_get_deps(main_db, dep_db) 
